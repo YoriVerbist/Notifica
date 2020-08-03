546361ee1539cfb0b71c25f659ac25117ad63aa4
Yori <yori.verbist1212@gmail.com>
2020-08-03 18:38:06 +0200

    First commit

---
+++ www/js/app.js
@@ -0,0 +1,15 @@
+$(function(){
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    $('.sidenav').sidenav();	/* https://materializecss.com/sidenav.html */
+
+    $('.sidenav a').click(function () {
+        $('.spa').hide();                           // Hides all div's when nav-bar is opened
+        $('#' + $(this).data('show')).show();       // Looks for what id is clicked and only shows that div
+        $('.sidenav').sidenav('close');             // Closes side-nav
+    });
+});
+
+function onDeviceReady() {
+    console.log('Device is ready');
+}
---
+++ www/js/todo.js
@@ -0,0 +1,8 @@
+let ToDo = function () {
+    let _todos = [];
+
+    let _setLocalStorage = function() {
+        console.log("Bewaar alle taken in 'todo'");
+        console.log(_todos);
+    }
+}
