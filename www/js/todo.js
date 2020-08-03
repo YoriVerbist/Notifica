2995119ef83f07f63743ab1d440ce2a9caf80ca6
Yori <yori.verbist1212@gmail.com>
2020-08-03 19:18:00 +0200

    Bug in adding fixed

--- www/js/todo.js
+++ www/js/todo.js
@@ -3,19 +3,28 @@ let ToDo = function(){
 
     let _setLocalStorage = function(){
         // Get all the todos that are already stored in local storage
-        localStorage.setItem('todo', JSON.stringify(_todos));  // localStorage.setItem('key', 'value')
+        localStorage.setItem('todo', JSON.stringify(_todos));
         _show();
     };
 
     let _show = function(){
-        return false;
+        $('#todos').empty();
+
+        for(let i = 0; i < _todos.length; i++) {
+            let task = `<li class="collection-item avatar"> 
+			<i class="material-icons circle red deleteTask" data-task="${i}">delete_forever</i>
+			<div class="title" data-task="${i}" contenteditable>${_todos[i]}</div>
+            </li>`;
+        
+            $('#todos').append(task);
+        }
     };
 
     let addTask = function(){
         console.log("Taak toevoegen todo");
 
         // Taak ophalen
-        let task = $('#task');
+        let task = $('#task').val();
 
 
         _todos.push(task);
