94e93a586448f35409bca4a74fc88e6fd6bc3ae2
Yori <yori.verbist1212@gmail.com>
2020-08-05 14:11:46 +0200

    _fireStore function fixed

--- www/js/boodschappen.js
+++ www/js/boodschappen.js
@@ -4,21 +4,26 @@ let Boodschappen = function(){
     let _fireStore = function(){
         db.collection("Boodschappen").get().then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
-                console.log(`${doc.id} => ${doc.data()}`);
+                _boodschappen.push(doc.data());
             });
         });
+
+        console.log(_boodschappen);
+        _show();
     };
 
     let _show = function(){
         $('#boodschappen').empty();
 
         for(let i = 0; i < _boodschappen.length; i++){
-            let boodschap = `<li class="collection-item avatar"> 
-            <div class="title" data-task="${i}">${_boodschappen[i]}</div>
-            <i class="material-icons circle red" id="deleteTask" data-task="${i}">delete_forever</i>
-            </li>`;
-
-            $('#boodschappenList').append(boodschap);
+            if (!_boodschappen.contains(_boodschappen[i])) {
+                let boodschap = `<li class="collection-item avatar"> 
+                    <div class="title" data-task="${i}">${_boodschappen[i].Datum}</div>
+                    <i class="material-icons circle red" id="deleteBoodschap" data-task="${i}">delete_forever</i>
+                    </li>`;
+                    console.log(_boodschappen[i].Datum);
+                $('#boodschappenList').append(boodschap);
+            }
         }
     };
 
@@ -38,10 +43,13 @@ let Boodschappen = function(){
         .catch(function(error) {
             console.error("Error writing document: ", error);
         });
+
+        _fireStore();
     };
 
     let init = function(){
         console.log('de boodschappen app start op...');
+        _fireStore();
     };
 
     return {
